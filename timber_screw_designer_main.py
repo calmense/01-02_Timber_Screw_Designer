1dc257059eedd84d2e171e779b667b468b8eacaa
Cal Mense <104301991+calmense@users.noreply.github.com>
2024-01-30 22:01:03 +0100

    Update timber_screw_designer_main.py

--- timber_screw_designer_main.py
+++ timber_screw_designer_main.py
@@ -8,7 +8,7 @@ from würth_screws_functions import get_length, ec5_87_tragfähigkeit_vg, get_mi
 
 # HTML Einstellungen
 # st.set_page_config(page_title="Timber Screw Designer", page_icon="🧊", layout="wide", theme="light")
-st.set_page_config(page_title="Timber Screw Designer")
+st.set_page_config(layout="wide")
 st.markdown("""<style>
 [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {width: 500px;}
 [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {width: 500px;margin-left: -500px;}
